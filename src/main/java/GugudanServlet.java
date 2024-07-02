c43575522a0069d46107f240f619747335f79d8e
kyungmin <ohkm9926@naver.com>
2024-07-02 11:43:50 +0900

    JSP에서 Rq 도입

--- src/main/java/GugudanServlet.java
+++ src/main/java/GugudanServlet.java
@@ -10,16 +10,17 @@ import java.io.IOException;
 public class GugudanServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("UTF-8");
-        resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html; charset-utf-8");
-
-        int dan = Integer.parseInt(req.getParameter("dan"));
-        int limit = Integer.parseInt(req.getParameter("limit"));
+//        req.setCharacterEncoding("UTF-8");
+//        resp.setCharacterEncoding("UTF-8");
+//        resp.setContentType("text/html; charset-utf-8");
+        Rq rp = new Rq(req, resp);
 
 
+        int dan = rp.getIntParam("dan" , 0);
+        int limit = rp.getIntParam("limit" , 0);
 
-        resp.getWriter().append("<h1>%d단</h1>".formatted(dan));
+        rp.appendBody("<h1>%d단 </h1>".formatted(dan));
+        
         for (int i =1; i <= limit; i++) {
             resp.getWriter().append("<div>%d * %d =%d</div>".formatted(dan, i, dan * i));
 
