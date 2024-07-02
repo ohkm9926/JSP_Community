8af2947ed1da506ed4d3bc10be9d8fe3f37a8387
kyungmin <ohkm9926@naver.com>
2024-07-02 11:07:57 +0900

    서블릿으로 구구단 구현

--- src/main/java/GugudanServlet.java
+++ src/main/java/GugudanServlet.java
@@ -6,14 +6,25 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset-utf-8");
 
-        resp.getWriter().append("안녕하세요. 반갑습니다");
+        int dan = Integer.parseInt(req.getParameter("dan"));
+        int limit = Integer.parseInt(req.getParameter("limit"));
+
+
+
+        resp.getWriter().append("<h1>%d단</h1>".formatted(dan));
+        for (int i =1; i <= limit; i++) {
+            resp.getWriter().append("<div>%d * %d =%d</div>".formatted(dan, i, dan * i));
+
+
+
+        }
     }
 }
