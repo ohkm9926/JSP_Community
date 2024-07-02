1e5b27a8115354e07fbd762b4f46ca2c166dc228
kyungmin <ohkm9926@naver.com>
2024-07-02 14:51:46 +0900

    db연동하기

---
+++ src/main/java/com/sbs/exam/servlet/ArticleListServlet.java
@@ -0,0 +1,49 @@
+package com.sbs.exam.servlet;
+
+import com.sbs.exam.util.DBUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet("/article/list")
+public class ArticleListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // DB 연결시작
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.printf("[ClassNotFoundException], %s",e.getMessage());
+            System.out.println("DB 드라이버 클래스 로딩실패");
+            return;
+        }
+
+        String url = "jdbc:mysql://127.0.0.1:3306/JSP_Community?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+        String user = "root";
+        String password = "1234";
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        // DB 연결 끝
+    }
+}
